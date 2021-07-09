b81e34b43dece7f399313a3f0ab279e637cf76e7
netajik <kancharapunetaji@gmail.com>
2021-07-09 21:27:13 +0530

    project initiated

---
+++ src/components/ConformationReservation.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default class ConformationReservation {
+
+    render() {
+        return (<div></div>)
+    }
+}
---
+++ src/components/FindFlights.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default class FindFlights {
+
+    render() {
+        return (<div></div>)
+    }
+}
